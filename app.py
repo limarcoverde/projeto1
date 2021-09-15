2bdbc0fba17d4cd6fa385faa8e26084ac8b1e55c
MARCELO LIMA ARCOVERDE <42537683+limarcoverde@users.noreply.github.com>
2021-09-14 23:38:25 -0300

    sdfsdfs

--- app.py
+++ app.py
@@ -10,8 +10,31 @@ import dash_auth
 import pandas as pd
 import numpy as np
 
-from figs import *
-from layout import *
+df = pd.read_csv('sales_with_extra_features.csv')
+df['DATA'] = pd.to_datetime(df['DATA'])
+
+DF_MES = df.loc[df.DATA.dt.to_period('Y')=='2019'].VENDAS.groupby(df.DATA.dt.to_period('M')).agg('mean')
+DF_TRI = df.loc[df.DATA.dt.to_period('Y')=='2019'].VENDAS.groupby(df.DATA.dt.to_period('Q')).agg('mean')
+DF_ANO = df.VENDAS.groupby(df.DATA.dt.to_period('Y')).agg('mean')
+
+
+fig_1 = go.Figure()
+fig_1.add_trace(go.Bar(x=DF_MES.index.astype('str'),y=DF_MES.values,name='Mês'))
+fig_1.add_trace(go.Scatter(x=DF_MES.index.astype('str'),y=[DF_TRI[int(i/4)] for i in range(12)],name='Trimestre'))
+
+
+
+fig_2 = make_subplots(rows=1,cols=2,column_widths=[0.7,0.3])
+
+fig_2.update_layout(title='Faturamento por Trimestre')
+fig_2.add_trace(go.Bar(x=DF_TRI.index.astype('str'),y=DF_TRI.values,name='Trimestre'),
+                            row=1, col=1)
+
+
+fig_2.update_layout(title='Faturamento por Ano')
+fig_2.add_trace(go.Bar(y=DF_ANO.index.astype('str'),x=DF_ANO.values,orientation='h',name='Ano'),
+                row=1, col=2)
+
 
 pio.templates.default = "plotly_white"
 
@@ -35,6 +58,85 @@ auth = dash_auth.BasicAuth(
 )
 
 app.title = "Sales Analytics!"
+
+def CreateHeader():
+        return html.Div(
+                className="header",
+                children=[
+                        #html.P(children="🔥", className="header-emoji"),
+                        html.H1(
+                                className="header-title",
+                                children="📈Sales Analytics", 
+                        ),
+                        html.P(
+                                className="header-description",
+                                children="Analise de vendas de um restaurante localizado no Rio Grande do sul no periodo de 2 anos.",
+                        ),
+                ],
+        )        
+
+     
+def CreateSlider(id,title,min,max,value,marks):
+        return html.Div(
+                children=[
+                        html.Div(children=title, className="menu-title"),
+                        dcc.RangeSlider(
+                                id=id,#'year-slider',
+                                min=min,
+                                max=max,
+                                value=value,
+                                marks=marks,
+                                step=None
+                        )
+                ],
+        )       
+
+def CreateDropDownMenu(id,title,options,value):     
+        return html.Div(
+                children=[
+                        html.Div(children=title, className="menu-title"),
+                        dcc.Dropdown(
+                                id=id,#"period-filter",
+                                options=options,#[{"label": period, "value": period} for period in ['M','Q','Y']],
+                                value=value,#"M",
+                                clearable=False,
+                                searchable=False,
+                                className="dropdown",
+                        ),
+                ],
+        )
+
+
+
+
+def CreateMenu(children,className="menu-body"):
+        return html.Div(
+                className=className,
+                children=children
+        )
+
+
+def CreateGraph(id,title,fig=go.Figure()):
+        return html.Div(
+                className="wrapper",
+                children=[
+                        
+                        html.Div(
+                                className="card",
+                                children=[
+                                        html.Div(children=title, className="menu-card"),
+                                        dcc.Graph(
+                                        id=id,
+                                        config={"displayModeBar": False},
+                                        figure=fig
+                                        )
+                                ]
+                        )
+                ]
+        )
+
+
+
 
 app.layout = html.Div(
         children=[
